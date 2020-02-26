e66a3d9e1937460ff03fac52631d9f73a9432d78
sk261 <sk261@humboldt.edu>
2020-02-26 10:37:15 -0800

    Rough outline of how these trees will look completed. Needs cleaning up.

--- Assets/Scripts/TreeBuilder.cs
+++ Assets/Scripts/TreeBuilder.cs
@@ -14,29 +14,65 @@ public class TreeBuilder : MonoBehaviour
     void Start()
     {
         tree = new loadTree(filePath);
-        GameObject parent = drawBranch(tree.trunk);
-        drawBranch(tree.trunk.branches[0], parent);
+        GameObject parent = drawBranch(tree.trunk, new Vector3(0,0,0));
     }
 
-    private GameObject drawBranch(loadTree.branch br, GameObject parent = null)
+    private GameObject drawBranch(loadTree.branch br, Vector3 origin, float parent_zaxisR = 0, float parent_yaxisR = 0)
     {
         GameObject obj;
         if (br.isFile) obj = leaf;
         else obj = branch;
 
-        Vector3 parentLoc = new Vector3(0f,0f,0f);
-        Quaternion parentRotation = new Quaternion(1f,1f,1f,1f);
-        if (parent != null)
-        {
-            float tipOffset = parent.transform.localScale.y * 6;
-            parentLoc = parent.transform.position + obj.transform.up * tipOffset;
-            parentRotation = new Quaternion();
-        }
-        obj = Instantiate(obj, parentLoc, parentRotation);
-        
+        /* Size = object length and width
+         * Size ratio = Z-axis Rotation
+         * Name ratio = Y-axis Rotation
+         * Mod ratio = object colour intensity
+         * 
+         */
+        float rY = (float)((br.name_ratio * 360 / 180 * Mathf.PI) + parent_yaxisR) % (Mathf.PI / 2);
+        float rZ = (float)((br.size_ratio * 360 / 180 * Mathf.PI) + parent_zaxisR) % (Mathf.PI / 2);
+        float x, y, z, length;
+        // length is determined by file size in Megabytes
+        length = (float)(br.size / Mathf.Pow(10, 6));
+        z = Mathf.Sin(rY) * length + origin.z;
+        y = Mathf.Sin(rZ) * length + origin.y;
+        x = Mathf.Cos(rZ)*Mathf.Cos(rY) * length + origin.x;
+
+
+        Vector3 end = new Vector3(x, y, z);
+        // Create object
+        obj = Instantiate(obj);
+        obj.name = br.name + "_" + br.level.ToString();
+        createBetweenPoints(obj, origin, end, length);
+
+        // DEBUGGING
+        /*
+        GameObject debugOrigin = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        debugOrigin.name = obj.name + "_origin";
+        debugOrigin.transform.position = origin;
+        GameObject debugEnd = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        debugEnd.name = obj.name + "_end";
+        debugEnd.transform.position = end;
+        */
+
+
         obj.SetActive(true);
+        // if (br.level >= 0)
+        foreach (loadTree.branch n in br.branches)
+                drawBranch(n, end, rZ, rY);
 
         return obj;
+    }
+
+    private void createBetweenPoints(GameObject obj, Vector3 p1, Vector3 p2, float length)
+    {
+        // Copied from Unity Forums
+        Vector3 pos = Vector3.Lerp(p1, p2, (float).5);
+        float dist = Vector3.Distance(p1, p2);
+        Vector3 newScale = new Vector3(dist/20, dist/2, dist/20);
+        obj.transform.localScale = newScale;
+        obj.transform.position = pos;
+        obj.transform.up = p2 - p1;
     }
 
 }
